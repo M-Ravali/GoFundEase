38711ce2e1421651493bffcc0508506bdc4bc4b8
Ravali <ravalimaddela14@gmail.com>
2024-06-04 22:51:23 -0500

    connected to server and db

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+const campaignRoutes = require('./routes/campaign');
+const cors = require('cors');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+console.log(`JWT_SECRET: ${process.env.JWT_SECRET}`);
+
+app.use(express.json());  
+
+app.use(cors());  
+
+app.use('/api/auth', authRoutes);
+app.use('/api/campaign', campaignRoutes);
+  
+const PORT = process.env.PORT || 5000;  
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));             
