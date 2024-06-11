2e0f10d305297823b65c6c73181946b06734bffa
Ravali <ravalimaddela14@gmail.com>
2024-06-11 11:20:04 -0500

    added routes in the server file

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/campaign', campaignRoutes);
-  
+   
 const PORT = process.env.PORT || 5000;  
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));             
