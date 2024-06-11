f72e3055d35c66c8119b127e77d9065ca5bf7d5e
Ravali <ravalimaddela14@gmail.com>
2024-06-11 11:18:23 -0500

    added routes and api call fn

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { registerUser, authUser } = require('../controllers/authController');
+const { registerUser, authUser, requestPasswordReset, resetPassword } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', authUser);
+router.post('/request-password-reset', requestPasswordReset);
+router.post('/reset-password/:token', resetPassword);
 
-module.exports = router;
+module.exports = router;  
