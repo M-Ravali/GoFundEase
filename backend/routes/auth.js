e98f996501c3c03a42630fb7bbd7748b0fcb8248
Sai Ramya Valleru <sairamya.valleru@slu.edu>
2024-06-13 14:12:19 -0500

    Changes made in routes to connect with profile.html

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,23 @@
-const express = require('express');
-const { registerUser, authUser, requestPasswordReset, resetPassword } = require('../controllers/authController');
+const express = require("express");
+const {
+  registerUser,
+  authUser,
+  requestPasswordReset,
+  resetPassword,
+} = require("../controllers/authController");
+
+const { getUserProfile } = require("../controllers/authController");
+const { getUserDonations } = require("../controllers/donationController");
+const { protect } = require("../middleware/authMiddleware"); // assuming you have a middleware to protect routes
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', authUser);
-router.post('/request-password-reset', requestPasswordReset);
-router.post('/reset-password/:token', resetPassword);
+router.post("/register", registerUser);
+router.post("/login", authUser);
+router.post("/request-password-reset", requestPasswordReset);
+router.post("/reset-password/:token", resetPassword);
 
-module.exports = router;  
+router.get("/profile", protect, getUserProfile);
+router.get("/donations", protect, getUserDonations);
+
+module.exports = router;
