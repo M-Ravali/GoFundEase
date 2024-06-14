090b1f458a6a8eca44b6837d1d71a483f80bb3d7
walash <walashspecial@hotmail.com>
2024-06-14 12:36:39 -0500

    volunteerCountroller to backend

---
+++ backend/controllers/volunteerController.js
@@ -0,0 +1,20 @@
+// controllers/volunteerController.js
+
+const Volunteer = require('./__test__/volunteer');
+
+exports.createVolunteer = async (req, res) => {
+    const { name, email, subject, message } = req.body;
+
+    try {
+        const newVolunteer = await Volunteer.create({ name, email, subject, message });
+
+        if (newVolunteer) {
+            res.status(200).json({ message: 'Volunteer information saved', volunteer: newVolunteer });
+        } else {
+            res.status(400).json({ message: 'Invalid volunteer data' });
+        }
+    } catch (error) {
+        console.error('Error creating volunteer:', error);
+        res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
+};
