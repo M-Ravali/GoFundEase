c11a5d7360fa30a8545b81cfe11ec95d05d3726f
Navreet20 <navreet.kaur@slu.edu>
2024-06-17 00:51:11 -0500

    stripe controller changed

--- backend/controllers/stripeController.js
+++ backend/controllers/stripeController.js
@@ -1,30 +1,29 @@
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
-
-
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(
+  "sk_test_51PSRzbEoECBkrKTdYfEgvPjG8U82QxEjb4vXzvWqVhsqNpHZkPKL7T954y9haWvyUMyci9rAAVNFZcHin3CAZFpH00aArekdSN"
+);
 
 exports.createPaymentIntent = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    const { amount } = req.body;
+  const { amount } = req.body;
 
-    try {
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount,
-            currency: 'usd',
-        });
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount * 100, //stripe accepts amount in cents
+      currency: "usd",
+    });
 
-        res.send({
-            clientSecret: paymentIntent.client_secret,
-            message: "Payment Intent Created Successfully"
-        });
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+      message: "Payment Intent Created Successfully",
+    });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 };
-
--- backend/index.js
+++ backend/index.js
@@ -49,3 +49,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 
+
